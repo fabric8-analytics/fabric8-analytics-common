2976c697c723628e2d3e57a02cde3aaf6861c0a2
Michal Srb <michal@redhat.com>
2017-03-27 17:05:56 +0200

    Bypass virtualenv generated shebangs which can be too long
    Fixes:
    + pip3 install -r requirements.txt
    ./runtest.sh: /home/jenkins/workspace/baytemp_ci-test_master-PG24IHBUAZ3FPFDRHE5GJDJSTQA6RJCNCSOVGPUES2PBLCQVBJAQ@2/integration-tests/venv/bin/pip3: /home/jenkins/workspace/baytemp_ci-test_master-PG24IHBUAZ3FPFDRHE5GJDJSTQA6RJ: bad interpreter: Permission denied
    + exit 1

--- integration-tests/runtest.sh
+++ integration-tests/runtest.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -ex
 
 function prepare_venv() {
-	virtualenv -p python3 venv && source venv/bin/activate && pip3 install -r requirements.txt
+	virtualenv -p python3 venv && source venv/bin/activate && python3 `which pip3` install -r requirements.txt
 }
 
 [ "$NOVENV" == "1" ] || prepare_venv || exit 1
 
-PYTHONDONTWRITEBYTECODE=1 behave --tags=-skip -D dump_errors=true @feature_list.txt $@
+PYTHONDONTWRITEBYTECODE=1 python3 `which behave` --tags=-skip -D dump_errors=true @feature_list.txt $@
