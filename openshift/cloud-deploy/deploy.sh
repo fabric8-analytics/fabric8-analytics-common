e6adae19b7a3b893ed285b33ad4198faa122c703
Michal Srb <michal@redhat.com>
2017-05-16 12:48:23 +0200

    Deploy secrets first

--- openshift/cloud-deploy/deploy.sh
+++ openshift/cloud-deploy/deploy.sh
@@ -3,7 +3,9 @@
 # We use $PTH_ENV in ../deploy.sh
 source conf
 
-${CLOUD_DEPLOYER_PATH}/deploy.sh --conf conf --services services --secrets-template secrets-template-aws.yaml --secrets-deploy ../secrets-deploy.sh --system-deploy ../deploy.sh
-
+# We need to deploy these secrets first
 curl -O https://gitlab.cee.redhat.com/bayesian/secrets/raw/master/secrets-template.yaml
 ../secrets-deploy.sh --secrets-file secrets-template.yaml
+
+${CLOUD_DEPLOYER_PATH}/deploy.sh --conf conf --services services --secrets-template secrets-template-aws.yaml --secrets-deploy ../secrets-deploy.sh --system-deploy ../deploy.sh
+
