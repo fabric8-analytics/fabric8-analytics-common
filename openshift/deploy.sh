919f4c3aeda720995fa1627532eaf846e2cdc492
Michal Srb <michal@redhat.com>
2017-06-08 08:15:55 +0200

    Fetch templates for gremlin and anitya from new location

--- openshift/deploy.sh
+++ openshift/deploy.sh
@@ -7,17 +7,22 @@ function oc_process_apply() {
 
 here=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 templates_dir="${here}/templates"
-templates="jobs server data-model worker pgbouncer recommender"
-templates_extra="gremlin anitya"
+f8a_templates="jobs server data-model worker pgbouncer recommender"
+other_templates="gremlin-docker anitya-docker"
 
 # generate and apply the ConfigMap
 . ${here}/generate-config.sh
 oc apply -f ${here}/config.yaml
 
 # get templates from fabric8-analytics GH organization
-for template in $templates
+for template in $f8a_templates
 do
   curl -sS https://raw.githubusercontent.com/fabric8-analytics/fabric8-analytics-${template}/master/openshift/template.yaml > ${templates_dir}/${template}.yaml
+done
+
+for template in $other_templates
+do
+  curl -sS https://raw.githubusercontent.com/fabric8-analytics/${template}/master/openshift/template.yaml > ${templates_dir}/${template}.yaml
 done
 
 # get gremlin template
@@ -26,8 +31,8 @@ curl -sS  https://raw.githubusercontent.com/containscafeine/data-model/master/gr
 curl -sS https://raw.githubusercontent.com/bkabrda/anitya-docker/master/openshift/template.yaml > ${templates_dir}/anitya.yaml
 
 oc_process_apply ${templates_dir}/pgbouncer.yaml
-oc_process_apply ${templates_dir}/gremlin.yaml "-v CHANNELIZER=http -v REST_VALUE=1"
-oc_process_apply ${templates_dir}/anitya.yaml
+oc_process_apply ${templates_dir}/gremlin-docker.yaml "-v CHANNELIZER=http -v REST_VALUE=1"
+oc_process_apply ${templates_dir}/anitya-docker.yaml
 oc_process_apply ${templates_dir}/recommender.yaml
 oc_process_apply ${templates_dir}/data-model.yaml
 oc_process_apply ${templates_dir}/worker.yaml "-v WORKER_ADMINISTRATION_REGION=ingestion"
