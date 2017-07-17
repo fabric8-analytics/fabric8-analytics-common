61abf86d203ea3276008d53868c2c6d078c5c0d1
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-07-17 12:27:02 +0200

    Remember HTTP response object + data + status

--- integration-tests/features/steps/common.py
+++ integration-tests/features/steps/common.py
@@ -160,7 +160,8 @@ def wait_for_stack_analysis_completion(context, version="1"):
     url = urljoin(stack_analysis_endpoint(context, version), id)
 
     for _ in range(timeout//sleep_amount):
-        status_code = requests.get(url).status_code
+        context.response = requests.get(url)
+        status_code = context.response.status_code
         if status_code == 200:
             break
         elif status_code != 202:
