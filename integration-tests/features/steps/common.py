2e83a6679026e35669a2d40b982bf03c4965fce3
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-08-16 12:46:06 +0200

    Test step to access generate-token endpoint

--- integration-tests/features/steps/common.py
+++ integration-tests/features/steps/common.py
@@ -485,6 +485,13 @@ def clean_all_failed_jobs(context, token="without"):
         context.response = requests.delete(url)
 
 
+@when('I access the job service endpoint to generate token')
+def job_service_generate_token(context):
+    url = "{jobs_api_url}api/v1/generate-token".format(
+            jobs_api_url=context.jobs_api_url)
+    context.response = requests.get(url)
+
+
 @when("I ask for analyses report for ecosystem {ecosystem}")
 def access_analyses_report(context, ecosystem):
     """API call to get analyses report for selected ecosystem."""
