456caa1d85e351872b838ab9113e33b8e5cca66a
Michal Srb <michal@redhat.com>
2017-08-24 10:48:47 +0200

    Add Dockerfile and script for running the tests in CentOS CI

---
+++ integration-tests/cico_run_tests.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -ex
+
+F8A_API_URL=${F8A_API_URL:-https://recommender.api.prod-preview.openshift.io}
+F8A_JOB_API_URL=${F8A_JOB_API_URL:-http://bayesian-jobs-bayesian-preview.b6ff.rh-idev.openshiftapps.com}
+F8A_ANITYA_API_URL=${F8A_ANITYA_API_URL:-not-used}
+RECOMMENDER_API_TOKEN=${RECOMMENDER_API_TOKEN:-not-set-in-e2e-tests}
+
+docker build -t f8a-e2e-tests .
+
+docker run -t \
+    -e F8A_API_URL=${F8A_API_URL} \
+    -e F8A_JOB_API_URL=${F8A_JOB_API_URL} \
+    -e F8A_ANITYA_API_URL=${F8A_ANITYA_API_URL} \
+    -e RECOMMENDER_API_TOKEN=${RECOMMENDER_API_TOKEN} \
+    f8a-e2e-tests --tags=-jobs.requires_auth $@
+
