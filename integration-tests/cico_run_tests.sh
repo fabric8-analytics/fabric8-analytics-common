798a8418aeb7ecf5654fac740920ce0f30cdd009
Michal Srb <michal@redhat.com>
2017-08-28 17:58:03 +0200

    Use API token in CentOS CI

--- integration-tests/cico_run_tests.sh
+++ integration-tests/cico_run_tests.sh
@@ -2,6 +2,14 @@
 
 set -ex
 
+if [ -e "jenkins-env" ]; then
+    cat jenkins-env \
+      | grep -E "(RECOMMENDER_API_TOKEN|JENKINS_URL|GIT_BRANCH|GIT_COMMIT|BUILD_NUMBER|ghprbSourceBranch|ghprbActualCommit|BUILD_URL|ghprbPullId)=" \
+      | sed 's/^/export /g' \
+      > ~/.jenkins-env
+    source ~/.jenkins-env
+fi
+
 F8A_API_URL=${F8A_API_URL:-https://recommender.api.prod-preview.openshift.io}
 F8A_JOB_API_URL=${F8A_JOB_API_URL:-http://bayesian-jobs-bayesian-preview.b6ff.rh-idev.openshiftapps.com}
 F8A_ANITYA_API_URL=${F8A_ANITYA_API_URL:-not-used}
