33a21017e53fc6d0f4976ce8048fe0e19036b927
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-09-04 11:13:29 +0200

    Ability to wait for component analysis with auth.token

--- integration-tests/features/steps/common.py
+++ integration-tests/features/steps/common.py
@@ -176,7 +176,9 @@ def start_analysis_for_component(context, ecosystem, component, version):
 
 
 @when("I wait for {ecosystem}/{component}/{version} component analysis to finish")
-def finish_analysis_for_component(context, ecosystem, component, version):
+@when("I wait for {ecosystem}/{component}/{version} component analysis to finish "
+      "{token} authorization token")
+def finish_analysis_for_component(context, ecosystem, component, version, token='without'):
     """Try to wait for the component analysis to be finished.
 
     Current API implementation returns just two HTTP codes:
@@ -187,10 +189,15 @@ def finish_analysis_for_component(context, ecosystem, component, version):
     timeout = context.component_analysis_timeout  # in seconds
     sleep_amount = 10  # we don't have to overload the API with too many calls
 
+    use_token = parse_token_clause(token)
+
     url = component_analysis_url(context, ecosystem, component, version)
 
     for _ in range(timeout // sleep_amount):
-        status_code = requests.get(url).status_code
+        if use_token:
+            status_code = requests.get(url, headers=authorization(context)).status_code
+        else:
+            status_code = requests.get(url).status_code
         if status_code == 200:
             break
         elif status_code != 404:
