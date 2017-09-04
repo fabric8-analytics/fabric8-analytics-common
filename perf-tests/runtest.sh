af6bf5ae52e028594dbf991b2db441d0eb122c75
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-09-04 12:58:23 +0200

    Script to run perf. tests

---
+++ perf-tests/runtest.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -ex
+
+function prepare_venv() {
+	virtualenv -p python3 venv && source venv/bin/activate && python3 `which pip3` install -r requirements.txt
+}
+
+[ "$NOVENV" == "1" ] || prepare_venv || exit 1
+
+python3 -B src/perf-tests.py
