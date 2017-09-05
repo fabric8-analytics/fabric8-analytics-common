ed42de59fb9872f31061d933f018d12ffbcc40c9
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-09-05 11:50:06 +0200

    Benchmark for API calls called from single thread or via multiple threads

---
+++ perf-tests/src/benchmarks.py
@@ -0,0 +1,38 @@
+import time
+
+
+def measure(function_to_call, check_function, measurement_count, pause_time, thread_id):
+    measurements = []
+    for i in range(measurement_count):
+        t1 = time.clock()
+        retval = function_to_call()
+        assert check_function(retval)
+        t2 = time.clock()
+        delta = t2 - t1
+        if thread_id is not None:
+            print("    thread: #{t}    call {i}/{m}    {delta}".format(t=thread_id,
+                                                                       i=i + 1,
+                                                                       delta=delta,
+                                                                       m=measurement_count))
+        else:
+            print("    #{i}    {delta}".format(i=i + 1, delta=delta))
+        measurements.append(delta)
+        time.sleep(pause_time)
+    return measurements
+
+
+def core_api_benchmark(core_api, measurement_count, pause_time, thread_id=None):
+    return measure(lambda: core_api.get(),
+                   lambda retval: retval.status_code == 200, measurement_count, pause_time,
+                   thread_id)
+
+
+def jobs_api_benchmark(jobs_api, measurement_count, pause_time, thread_id=None):
+    return measure(lambda: jobs_api.get(),
+                   lambda retval: retval.status_code == 200, measurement_count, pause_time,
+                   thread_id)
+
+
+def core_api_benchmark_thread(core_api, measurement_count, pause_time, q, thread_id):
+    measurements = core_api_benchmark(core_api, measurement_count, pause_time, thread_id)
+    q.put(measurements)
