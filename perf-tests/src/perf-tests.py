4616fdefc1c7df8acccdd89ea792fa24ecfa9ca6
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-09-07 14:16:38 +0200

    Check environment variables before the tests are started

--- perf-tests/src/perf-tests.py
+++ perf-tests/src/perf-tests.py
@@ -1,4 +1,45 @@
+import json
+import time
+import datetime
+import subprocess
+import os.path
+import sys
+import queue
+import threading
+
+from coreapi import *
+from jobsapi import *
+import benchmarks
+import graph
+
+
+def check_environment_variable(env_var_name):
+    print("Checking: {e} environment variable existence".format(
+        e=env_var_name))
+    if os.environ.get(env_var_name) is None:
+        print("Fatal: {e} environment variable has to be specified"
+              .format(e=env_var_name))
+        sys.exit(1)
+    else:
+        print("    ok")
+
+
+def check_environment_variables():
+    environment_variables = [
+        "F8A_API_URL",
+        "F8A_JOB_API_URL",
+        "RECOMMENDER_API_TOKEN",
+        "JOB_API_TOKEN",
+        "AWS_ACCESS_KEY_ID",
+        "AWS_SECRET_ACCESS_KEY",
+        "S3_REGION_NAME"]
+    for environment_variable in environment_variables:
+        check_environment_variable(environment_variable)
+
+
 def main():
+    check_environment_variables()
+
     pass
 
 
