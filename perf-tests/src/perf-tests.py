19825955444f2f0e519ed009fc7c7ff2e745b568
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-09-07 14:17:29 +0200

    Check if the tested system is running

--- perf-tests/src/perf-tests.py
+++ perf-tests/src/perf-tests.py
@@ -37,8 +37,40 @@ def check_environment_variables():
         check_environment_variable(environment_variable)
 
 
+def is_system_running(core_api, jobs_api):
+    return core_api.is_api_running() and \
+           jobs_api.is_api_running()
+
+
+def check_system(core_api, jobs_api):
+    print("Checking: core API and JOBS API endpoints")
+    if not is_system_running(core_api, jobs_api):
+        print("Fatal: tested system is not available")
+        sys.exit(1)
+    else:
+        print("    ok")
+
+
 def main():
     check_environment_variables()
+
+    coreapi_url = os.environ.get('F8A_API_URL', None)
+    jobs_api_url = os.environ.get('F8A_JOB_API_URL', None)
+
+    recommender_api_token = os.environ.get('RECOMMENDER_API_TOKEN')
+    job_api_token = os.environ.get('JOB_API_TOKEN')
+
+    aws_access_key_id = os.environ.get('AWS_ACCESS_KEY_ID')
+    aws_secret_access_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    s3_region_name = os.environ.get('S3_REGION_NAME')
+    deployment_prefix = os.environ.get('DEPLOYMENT_PREFIX', 'STAGE')
+
+    core_api = CoreApi(coreapi_url, recommender_api_token)
+    jobs_api = JobsApi(jobs_api_url, job_api_token)
+    s3 = S3Interface(aws_access_key_id, aws_secret_access_key, s3_region_name, deployment_prefix)
+
+    check_system(core_api, jobs_api)
+
 
     pass
 
