c6bc46466f1e3789c91e6b8eabab772cac42710f
Pavel Kajaba <pkajaba@redhat.com>
2017-10-03 10:09:33 +0200

    [Postgres] Remove specifing of file to patch

--- postgres-docker/update_common.sh
+++ postgres-docker/update_common.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl -o common.sh https://raw.githubusercontent.com/sclorg/postgresql-container/master/9.4/root/usr/share/container-scripts/postgresql/common.sh && patch common.sh < multiple_dbs.patch
+curl -o common.sh https://raw.githubusercontent.com/sclorg/postgresql-container/master/9.4/root/usr/share/container-scripts/postgresql/common.sh && patch < multiple_dbs.patch
 
