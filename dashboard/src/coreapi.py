9129a9302788bdeeb58b7c079fc4b3a11249640f
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-10-19 15:03:36 +0200

    Updated docstrings in core API module

--- dashboard/src/coreapi.py
+++ dashboard/src/coreapi.py
@@ -1,18 +1,21 @@
+"""Module with class representing core (server) API."""
 from api import *
 import time
-import datetime
-import json
 
 
 class CoreApi(Api):
+    """Class representing core (server) API."""
 
     def __init__(self, url, token):
+        """Set the API endpoint and store the authorization token if provided."""
         super().__init__(url, token)
 
     def authorization(self):
+        """Return a HTTP header with authorization token."""
         return {'Authorization': 'Bearer {token}'.format(token=self.token)}
 
     def check_auth_token_validity(self):
+        """Check that the authorization token is valid by calling the API and check HTTP code."""
         endpoint = self.url + 'api/v1/component-search/foobar'
         response = requests.get(endpoint, headers=self.authorization())
         if response.status_code != 200:
