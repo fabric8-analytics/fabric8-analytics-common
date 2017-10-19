d3d483c6b5433a7fbb3fb13a9ccab55e2cc31bff
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-10-19 15:08:12 +0200

    Class Results should provide more info about the system

--- dashboard/src/results.py
+++ dashboard/src/results.py
@@ -1,13 +1,26 @@
+"""Results gathered by the Dashboard to be published."""
+
+
 class Results():
+    """Class representing results gathered by the Dashboard to be published."""
 
     def __init__(self):
-        self.core_api = False
-        self.jobs_api = False
-        self.core_api_auth_token = False
-        self.jobs_api_auth_token = False
+        """Prepare empty result structure."""
+        self.stage = {}
+        self.production = {}
+        self.repositories = {}
+        self.repo_statistics = {}
+        self.repo_linter_checks = {}
+        self.repo_docstyle_checks = {}
+        self.repo_prefix = "https://github.com/fabric8-analytics/"
+        self.source_files = {}
 
     def __repr__(self):
-        template = "Core API: {c}\nJobs API: {j}\n" + \
-                   "Core API auth: {ca}\nJobs API auth: {ja}"
-        return template.format(c=self.core_api, j=self.jobs_api,
-                               ca=self.core_api_auth_token, ja=self.jobs_api_auth_token)
+        """Return textual representation of all results."""
+        template = "Stage: {stage}\nProduction: {production}\n" + \
+                   "Repo stats: {rs}\nRepo linter checks: {rl}\n Repo docstyle checks: {rd}"
+        return template.format(stage=self.stage,
+                               production=self.production,
+                               rs=self.repo_statistics,
+                               rl=self.repo_linter_checks,
+                               rd=self.repo_docstyle_checks)
