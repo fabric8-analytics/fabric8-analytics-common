66bea70051baacc4b70d4b2451c482956f1ccea3
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-10-23 13:20:41 +0200

    Gathering detailed information about source files

--- dashboard/src/dashboard.py
+++ dashboard/src/dashboard.py
@@ -151,18 +151,33 @@ def parse_docstyle_results(repository):
     return parse_linter_results(repository + ".pydocstyle")
 
 
+def parse_line_count(line):
+    line = line.strip()
+    line_count, filename = line.split(" ")
+    # remove prefix that is not relevant much
+    if filename.startswith("./"):
+        filename = filename[len("./"):]
+    return line_count, filename
+
+
 def get_source_files(repository):
     command = ("pushd {repo}; wc -l `find . -path ./venv -prune -o -name '*.py' -print` " +
                "| head -n -1 > ../{repo}.count;popd").format(repo=repository)
     os.system(command)
-    files = {}
+    filenames = []
+    line_counts = {}
     count = 0
 
     with open("{repo}.count".format(repo=repository)) as fin:
         for line in fin:
             count += 1
+            line_count, filename = parse_line_count(line)
+            filenames.append(filename)
+            line_counts[filename] = line_count
 
-    return {"count": count}
+    return {"count": count,
+            "filenames": filenames,
+            "line_counts": line_counts}
 
 
 def update_overall_status(results, repository):
