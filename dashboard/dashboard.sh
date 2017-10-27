8f4229f1aa922bce1733a55ddd7df37cabbf4ac9
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-10-27 10:21:35 +0200

    Dashboard should run smoketests and the script should not fail, even if smoketests fail

--- dashboard/dashboard.sh
+++ dashboard/dashboard.sh
@@ -1,10 +1,8 @@
-#!/bin/bash -ex
+#!/bin/bash -x
 
 function prepare_venv() {
 	virtualenv -p python3 venv && source venv/bin/activate && python3 `which pip3` install -r requirements.txt
 }
-
-[ "$NOVENV" == "1" ] || prepare_venv || exit 1
 
 export F8A_API_URL_STAGE=http://bayesian-api-bayesian-preview.b6ff.rh-idev.openshiftapps.com
 export F8A_API_URL_PROD=https://recommender.api.openshift.io
@@ -21,5 +19,22 @@ export F8A_JOB_API_URL_PROD=http://bayesian-jobs-bayesian-production.09b5.dsaas.
 #export AWS_ACCESS_KEY_ID=""
 #export AWS_SECRET_ACCESS_KEY=""
 export S3_REGION_NAME="us-east-1"
+
+function run_smoketests()
+{
+    cwd=`pwd`
+    pushd ../integration-tests/
+    export F8A_API_URL=$1
+    export F8A_JOB_API_URL=$2
+    export F8A_ANITYA_API_URL=not_used
+    ./runtest.sh --tags=smoketest > $cwd/$3
+    echo $? > $cwd/$4
+    popd
+}
+
+run_smoketests $F8A_API_URL_STAGE $F8A_JOB_API_URL_STAGE smoketests_stage.log smotests_stage.results
+run_smoketests $F8A_API_URL_PROD $F8A_JOB_API_URL_PROD smoketests_prod.log smotests_prod.results
+
+[ "$NOVENV" == "1" ] || prepare_venv || exit 1
 
 python3 -B src/dashboard.py $@
