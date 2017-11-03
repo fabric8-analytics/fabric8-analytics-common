28f5d2877684ae221a3473e77dbb4cceb42b83eb
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-11-03 11:27:23 +0100

    Updated docstrings

--- integration-tests/features/src/authorization_tokens.py
+++ integration-tests/features/src/authorization_tokens.py
@@ -2,10 +2,16 @@
 
 
 def jobs_api_authorization(context):
-    """Construct header with authorization token for the jobs API calls."""
+    """Construct header with authorization token for the jobs API calls.
+
+    Returned dict can be added to the 'request' object.
+    """
     return {'auth-token': '{token}'.format(token=context.jobs_api_token)}
 
 
 def authorization(context):
-    """Construct header with authorization token for the server API calls."""
+    """Construct header with authorization token for the server API calls.
+
+    Returned dict can be added to the 'request' object.
+    """
     return {'Authorization': 'Bearer {token}'.format(token=context.token)}
