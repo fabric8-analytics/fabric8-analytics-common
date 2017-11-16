8abf7be4c2567f00e2fb567d5174a56e6c6bc8e5
Michal Srb <michal@redhat.com>
2017-11-16 21:34:40 +0100

    Remove old cloud-deployer scripts and documentation

--- openshift/aws/allocate-aws-resources.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-${CLOUD_DEPLOYER_PATH}/deploy.sh --conf conf --services services --secrets-template secrets-template-aws.yaml --secrets-deploy ./dont-deploy.sh --system-deploy ./dont-deploy.sh
-
