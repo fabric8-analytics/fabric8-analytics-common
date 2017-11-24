fb5fc6e62a3c1b018d8f68cc37e4d541226a564b
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-11-24 11:31:37 +0100

    Test step for check the Gremlin response structure

--- integration-tests/features/steps/gremlin.py
+++ integration-tests/features/steps/gremlin.py
@@ -4,6 +4,7 @@ import requests
 
 from behave import given, then, when
 from urllib.parse import urljoin
+from src.json_utils import *
 
 
 @when('I access Gremlin API')
@@ -16,3 +17,36 @@ def post_query(context, query):
     """Post the already constructed query to the Gremlin."""
     data = {"gremlin": query}
     context.response = requests.post(context.gremlin_url, json=data)
+
+
+@then('I should get valid Gremlin response')
+def valid_gremlin_response(context):
+    """Check that the Gremlin response is valid."""
+    check_request_id_value_in_json_response(context, "requestId")
+
+    data = context.response.json()
+    assert data, "Gremlin does not send a proper response"
+
+    check_gremlin_status_node(data)
+    check_gremlin_result_node(data)
+
+
+def check_gremlin_status_node(data):
+    """Check the basic structure of the 'status' node in Gremlin response."""
+    status = check_and_get_attribute(data, "status")
+    message = check_and_get_attribute(status, "message")
+    code = check_and_get_attribute(status, "code")
+    attributes = check_and_get_attribute(status, "attributes")
+
+    assert message == ""
+    assert code == 200
+
+
+def check_gremlin_result_node(data):
+    """Check the basic structure of the 'result' node in Gremlin response."""
+    result = check_and_get_attribute(data, "result")
+    data = check_and_get_attribute(result, "data")
+    meta = check_and_get_attribute(result, "meta")
+
+    assert type(data) is list
+    assert type(meta) is dict
