587b36657de696106ce83d0a4fde2f6d434268ea
Pavel Tisnovsky <ptisnovs@redhat.com>
2017-12-01 12:02:47 +0100

    Provide environment variable with URL to the Gremlin service

--- integration-tests/cico_setup.sh
+++ integration-tests/cico_setup.sh
@@ -23,3 +23,4 @@ prep
 F8A_API_URL=${F8A_API_URL:-https://recommender.api.prod-preview.openshift.io}
 F8A_JOB_API_URL=${F8A_JOB_API_URL:-http://bayesian-jobs-bayesian-preview.b6ff.rh-idev.openshiftapps.com}
 F8A_ANITYA_API_URL=${F8A_ANITYA_API_URL:-not-used}
+F8A_GREMLIN_URL=${F8A_GREMLIN_URL:-http://bayesian-gremlin-http-preview-b6ff-bayesian-preview.b6ff.rh-idev.openshiftapps.com}
