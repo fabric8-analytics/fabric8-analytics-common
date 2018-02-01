9e292926fb3f42ff5ba7919f3889d346bd38b8a2
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-02-01 09:03:21 +0100

    New checks for schema endpoints

--- integration-tests/features/steps/server_api.py
+++ integration-tests/features/steps/server_api.py
@@ -4,6 +4,8 @@ import requests
 
 from src.utils import *
 from src.authorization_tokens import *
+from src.attribute_checks import *
+from src.schema_validator import *
 
 
 @when('I access {url:S}')
@@ -39,3 +41,51 @@ def check_versions(context, num=0, versions='', ecosystem='', package=''):
         assert v['ecosystem'] == ecosystem
         assert v['package'] == package
         assert v['version'] in versions
+
+
+@then('I should find the endpoint {endpoint} in the list of supported endpoints')
+def check_endpoint_in_paths(context, endpoint):
+    """Check the existence of given endpoint in the list of all supported endpoints."""
+    data = context.response.json()
+    paths = check_and_get_attribute(data, "paths")
+    assert endpoint in paths, "Cannot find the expected endpoint {e}".format(e=endpoint)
+
+
+@then('I should find the schema {schema} version {version} in the list of supported schemas')
+def check_schema_existence(context, schema, version, selector=None):
+    """Check the existence of given schema."""
+    data = context.response.json()
+    if selector is not None:
+        api_schemas = check_and_get_attribute(data, selector)
+        schema = check_and_get_attribute(api_schemas, schema)
+    else:
+        schema = check_and_get_attribute(data, schema)
+    check_and_get_attribute(schema, version)
+
+
+@then('I should find the schema {schema} version {version} in the list of supported schemas '
+      'for API calls')
+def check_schema_existence_api_call(context, schema, version):
+    """Check the existence of given schema (API calls)."""
+    check_schema_existence(context, schema, version, "api")
+
+
+@then('I should find the schema {schema} version {version} in the list of component analyses '
+      'schemas')
+def check_schema_existence_component_analyses(context, schema, version):
+    """Check the existence of given schema (component analyses)."""
+    check_schema_existence(context, schema, version, "component_analyses")
+
+
+@then('I should find the schema version {version} in the list of schema versions')
+def check_schema_version(context, version):
+    """Check the existence of schema version."""
+    data = context.response.json()
+    check_and_get_attribute(data, version)
+
+
+@then('I should find valid schema in the server response')
+def check_valid_schema(context):
+    """Check if the schema is valid, validation is performed against metaschema."""
+    data = context.response.json()
+    validate_schema(data)
