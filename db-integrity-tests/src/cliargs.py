13f3b7a83135493d93d2649f442adf62a481ec91
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-03-14 09:55:02 +0100

    New CLI option: --log-level

--- db-integrity-tests/src/cliargs.py
+++ db-integrity-tests/src/cliargs.py
@@ -3,3 +3,8 @@
 import argparse
 
 cli_parser = argparse.ArgumentParser()
+
+cli_parser.add_argument('--log-level',
+                        help='log level as defined in ' +
+                             'https://docs.python.org/3.5/library/logging.html#logging-level',
+                        type=int, default=20)
