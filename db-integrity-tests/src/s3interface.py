1f3593f439a0280c0ade75521ad0ad537495438c
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-03-14 09:56:15 +0100

    Methods to retrieve list of all ecosystems from the S3

--- db-integrity-tests/src/s3interface.py
+++ db-integrity-tests/src/s3interface.py
@@ -1,4 +1,5 @@
 """AWS S3 Interface used by tests."""
+
 import boto3
 import botocore
 from botocore.exceptions import ClientError
@@ -134,6 +135,20 @@ class S3Interface():
         assert s3 is not None
         data = s3.Object(self.full_bucket_name(bucket_name), key).get()[attribute]
         return data
+
+    def read_ecosystems_from_bucket(self, bucket_name):
+        """Return list of all ecosystems from selected bucket."""
+        bucket = self.s3_resource.Bucket(self.full_bucket_name(bucket_name))
+        result = bucket.meta.client.list_objects(Bucket=bucket.name, Delimiter='/', Prefix='pypi/')
+        return [o.get('Prefix') for o in result.get('CommonPrefixes')]
+
+    def read_ecosystems_from_core_package_data(self):
+        """Return list of all ecosystems from core-package-data bucket."""
+        return self.read_ecosystems_from_bucket("bayesian-core-package-data")
+
+    def read_ecosystems_from_core_data(self):
+        """Return list of all ecosystems from core-data bucket."""
+        return self.read_ecosystems_from_bucket("bayesian-core-data")
 
     @staticmethod
     def selector_to_key(selector):
