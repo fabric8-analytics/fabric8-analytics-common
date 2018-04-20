1e5a37b838c29b2e158bc6bbbb98584d9324947f
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-04-20 13:26:51 +0200

    Loading repolist

--- dashboard/src/repositories.py
+++ dashboard/src/repositories.py
@@ -1,18 +1,16 @@
 """List of repositories to check."""
 
-repositories = [
-    "fabric8-analytics-common",
-    "fabric8-analytics-server",
-    "fabric8-analytics-worker",
-    "fabric8-analytics-jobs",
-    "fabric8-analytics-tagger",
-    "fabric8-analytics-stack-analysis",
-    "fabric8-analytics-license-analysis",
-    "fabric8-analytics-data-model",
-    "fabric8-analytics-recommender",
-    "fabric8-analytics-api-gateway",
-    "fabric8-analytics-nvd-toolkit",
-    "fabric8-analytics-auth",
-    "fabric8-gemini-server",
-    "f8a-server-backbone"
-]
+repolist = []
+
+
+class Repositories():
+    """Class representing list of repositories."""
+
+    def __init__(self, config):
+        """Load list of repositories from the provided config object."""
+        self._repolist = config.get_repolist()
+
+    @property
+    def repolist(self):
+        """Getter for the 'repolist' attribute."""
+        return self._repolist
