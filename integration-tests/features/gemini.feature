73ee04762af1ead84cdbb9ac6d366a7a3736cc64
Samuzzal Choudhury <samuzzal@redhat.com>
2018-04-30 21:22:56 +0530

    Incorporated review comments and fixed build error

--- integration-tests/features/gemini.feature
+++ integration-tests/features/gemini.feature
@@ -1,23 +1,23 @@
 Feature: Gemini Analytics API
 
   Scenario: Check the Gemini API /api/v1/register response
-    Given gemini service is running
-      And gemini service git url is https://github.com/jitpack/maven-simple
-      And gemini service git sha is 9466faa13d65044c8430b418327df826f13ca07a
-    When I post to gemini API api/v1/register
+    Given Gemini service is running
+      And Gemini service git url is https://github.com/jitpack/maven-simple
+      And Gemini service git sha is 9466faa13d65044c8430b418327df826f13ca07a
+    When I post to Gemini API api/v1/register
     Then I should get 401 status code
     When I acquire the authorization token
     Then I should get the proper authorization token
-    When I post to gemini API api/v1/register with authorization token
+    When I post to Gemini API api/v1/register with authorization token
     Then I should get 200 status code
 
   Scenario: Check the Gemini API /api/v1/report response
-    Given gemini service is running
-      And gemini service git url is https://github.com/jitpack/maven-simple
-      And gemini service git sha is 9466faa13d65044c8430b418327df826f13ca07a
-    When I get to gemini API api/v1/report
+    Given Gemini service is running
+      And Gemini service git url is https://github.com/jitpack/maven-simple
+      And Gemini service git sha is 9466faa13d65044c8430b418327df826f13ca07a
+    When I get to Gemini API api/v1/report
     Then I should get 401 status code
     When I acquire the authorization token
     Then I should get the proper authorization token
-    When I get to gemini API api/v1/report with authorization token
+    When I get to Gemini API api/v1/report with authorization token
     Then I should get 200 status code
--- integration-tests/features/steps/gemini.py
+++ integration-tests/features/steps/gemini.py
@@ -7,30 +7,31 @@ import json
 
 from behave import given, then, when
 
+from src.parsing import *
 from src.json_utils import *
 from src.authorization_tokens import *
 
 
-@given('gemini service is running')
+@given('Gemini service is running')
 def running_gemini_api(context):
-    """Check if 3scale pod is running."""
+    """Check if gemini pod is running."""
     return context.is_gemini_api_running
 
 
-@given('gemini service git url is {url}')
+@given('Gemini service git url is {url}')
 def set_git_url(context, url):
     """Set git url for test."""
     context.url = url
 
 
-@given('gemini service git sha is {sha}')
+@given('Gemini service git sha is {sha}')
 def set_git_url(context, sha):
     """Set git sha for test."""
     context.sha = sha
 
 
-@when('I {method} to gemini API {endpoint}')
-@when('I {method} to gemini API {endpoint} {token} authorization token')
+@when('I {method} to Gemini API {endpoint}')
+@when('I {method} to Gemini API {endpoint} {token} authorization token')
 def call_backbone_api(context, method="get", endpoint="/api/v1/register", token="without"):
     """Get or post data to gemini API."""
     use_token = parse_token_clause(token)
@@ -48,6 +49,9 @@ def call_backbone_api(context, method="get", endpoint="/api/v1/register", token=
         context.response = requests.post('{}/{}'.format(context.gemini_api_url, endpoint),
                                          json=json.loads(content), headers=headers)
     else:
-        api_url = "{}/{}?git-url={}&git-sha={}".format(context.gemini_api_url, endpoint,
-                                                       context.url, context.sha)
+        api_url = "{api_url}/{endpoint}?git-url={git_url}&git-sha={git_sha}".format(
+            api_url=context.gemini_api_url,
+            endpoint=endpoint,
+            git_url=context.url,
+            git_sha=context.sha)
         context.response = requests.get(api_url, headers=headers)
