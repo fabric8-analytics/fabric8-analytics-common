579d4a35cc8d4361dc365b4fcb48c0599500b52e
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-06-05 13:51:05 +0200

    Fix: the repository name needs to be updated

--- dashboard/src/git_utils.py
+++ dashboard/src/git_utils.py
@@ -3,15 +3,26 @@
 import os
 
 
+def update_repository_name(repository):
+    """Update given repository name so it won't contain any prefix(es)."""
+    lastSlash = repository.rfind("/")
+
+    # make sure we use just the repo name
+    if lastSlash >= 0 and lastSlash < len(repository) - 1:
+        return repository[1 + lastSlash:]
+    else:
+        return repository
+
+
 def is_repository_cloned(repository):
     """Check if the directory with cloned repository exist."""
-    return os.path.isdir(repository)
+    return os.path.isdir("repositories/" + update_repository_name(repository))
 
 
 def clone_repository(repository):
     """Clone the selected repository."""
     print("Cloning the repository {repository}".format(repository=repository))
-    prefix = "https://github.com/"
+    prefix = "https://github.com"
     command = "pushd repositories; git clone --single-branch --depth 1 {prefix}/{repo}.git; popd".\
         format(prefix=prefix, repo=repository)
     os.system(command)
@@ -19,6 +30,7 @@ def clone_repository(repository):
 
 def fetch_repository(repository):
     """Fetch the selected repository."""
+    repository = update_repository_name(repository)
     print("Fetching changes from the repository {repository}".format(repository=repository))
     command = "pushd repositories/{repository}; git fetch; popd".format(repository=repository)
     os.system(command)
