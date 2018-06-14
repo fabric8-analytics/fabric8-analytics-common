10a5bfa74da0b634dfab0d0f673a56397c07bd62
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-06-14 13:22:27 +0200

    Logging for unit_tests.py module

--- dashboard/src/unit_tests.py
+++ dashboard/src/unit_tests.py
@@ -4,6 +4,7 @@ import re
 import requests
 from progress_bar import *
 from jacoco_to_codecov import *
+from fastlog import log
 
 
 # default code coverage threshold used when no threshold is specified
@@ -20,14 +21,25 @@ def unit_test_coverage_ok(unit_test_coverage, code_coverage_threshold=CODE_COVER
         return False
 
 
+def log_coverage(statements, missed, coverage):
+    """Log info about the coverage read from reports."""
+    with log.indent():
+        log.info("statements {s}".format(s=statements))
+        log.info("missed     {m}".format(m=missed))
+        log.info("coverage   {c}".format(c=coverage))
+
+
 def parse_unit_test_statistic(line):
     """Parse the line containing unit test coverage statistic."""
     pattern = re.compile(r'TOTAL\s+(\d+)\s+(\d+)\s+(\d+)%')
     match = pattern.match(line)
     if len(match.groups()) == 3:
         coverage = match.group(3)
-        return {"statements": match.group(1),
-                "missed": match.group(2),
+        statements = match.group(1)
+        missed = match.group(2)
+        log_coverage(statements, missed, coverage)
+        return {"statements": statements,
+                "missed": missed,
                 "coverage": coverage,
                 "progress_bar_class": progress_bar_class(coverage),
                 "progress_bar_width": progress_bar_width(coverage)}
@@ -39,6 +51,7 @@ def compute_jacoco_test_statistic(project_coverage_report):
     """Compute test coverage etc. from the CSV data exported from JaCoCo and converted."""
     java_classes = project_coverage_report.read_java_classes()
     statements, missed, coverage = ProjectCoverageReport.compute_total(java_classes)
+    log_coverage(statements, missed, coverage)
 
     return {"statements": statements,
             "missed": missed,
@@ -83,8 +96,15 @@ def line_with_unit_test_summary(line, report_type="pycov"):
     return report_type == "pycov" and line.startswith("TOTAL      ") and line.endswith("%")
 
 
+def log_report_type(report_type):
+    """Display info which unit test report type has been detected."""
+    with log.indent():
+        log.info("{report_type} report detected".format(report_type=report_type))
+
+
 def read_unit_test_coverage(ci_jobs, jenkins_url, repository):
     """Read and process unit test coverage."""
+    log.info("Reading unit test coverage")
     url = ci_jobs.get_console_output_url(repository)
     report_type = None
     if url is not None:
@@ -96,9 +116,11 @@ def read_unit_test_coverage(ci_jobs, jenkins_url, repository):
                 line = line.strip()
                 # check where the test coverage begins
                 if line_with_unit_test_header(line):
+                    log_report_type("pycov")
                     report_type = "pycov"
                     unit_test_output.append(line)
                 elif line_with_jacoco_test_header(line):
+                    log_report_type("jacoco")
                     report_type = "jacoco"
                     # not needed to write the header
                     # unit_test_output.append(line)
@@ -118,6 +140,7 @@ def read_unit_test_coverage(ci_jobs, jenkins_url, repository):
                 # now we know we have something to report
                 elif report_type:
                     unit_test_output.append(line)
+    log.warning("No coverage report found")
     return None
 
 
