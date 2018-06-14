00bdb3a8a01026ca5116b68fe4b28756f7f6edf9
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-06-14 13:14:13 +0200

    Logging for source_files.py module

--- dashboard/src/source_files.py
+++ dashboard/src/source_files.py
@@ -2,6 +2,8 @@
 
 import os
 
+from fastlog import log
+
 
 def parse_line_count(line):
     """Parse the information with line counts."""
@@ -15,6 +17,7 @@ def parse_line_count(line):
 
 def get_source_files(repository):
     """Find all source files in the selected repository."""
+    log.info("Getting source files")
     command = ("pushd repositories/{repo} > /dev/null; " +
                r"wc -l `find . -path ./venv -prune -o \( -name '*.py' -o -name '*.java' \) " +
                "-print` | head -n -1 > ../../{repo}.count; " +
@@ -25,13 +28,21 @@ def get_source_files(repository):
     total_lines = 0
     count = 0
 
-    with open("{repo}.count".format(repo=repository)) as fin:
-        for line in fin:
-            count += 1
-            line_count, filename = parse_line_count(line)
-            filenames.append(filename)
-            line_counts[filename] = line_count
-            total_lines += line_count
+    with log.indent():
+        with open("{repo}.count".format(repo=repository)) as fin:
+            for line in fin:
+                with log.indent():
+                    log.debug(line)
+                count += 1
+                line_count, filename = parse_line_count(line)
+                filenames.append(filename)
+                line_counts[filename] = line_count
+                total_lines += line_count
+
+        log.debug("Files: {files}".format(files=count))
+        log.debug("Lines: {lines}".format(lines=total_lines))
+
+    log.success("Done")
 
     return {"count": count,
             "filenames": filenames,
