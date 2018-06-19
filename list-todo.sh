de02d48688e0d75c55eb51f9146444d31e81c7a5
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-06-19 13:48:33 +0200

    Fixed remaining issues

--- list-todo.sh
+++ list-todo.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
+
 directories="dashboard db-integrity-tests integration-tests perf-tests reproducers ui-tests"
 
 # checks for the whole directories
 for directory in $directories
 do
-    grep -r -n "TODO: " $directory
+    grep -r -n "TODO: " "$directory"
 done
