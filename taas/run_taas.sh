029e68bfc9514a1393992a43ed6b44d1698779c4
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-08-30 11:35:35 +0200

    Proper setup API endpoints

--- taas/run_taas.sh
+++ taas/run_taas.sh
@@ -7,9 +7,11 @@ function prepare_venv() {
 
 [ "$NOVENV" == "1" ] || prepare_venv || exit 1
 
+export F8A_API_URL=http://bayesian-api-bayesian-preview.b6ff.rh-idev.openshiftapps.com
+export F8A_JOB_API_URL=http://bayesian-jobs-bayesian-preview.b6ff.rh-idev.openshiftapps.com
 
 DIR=$(pwd)
 echo $DIR
 pushd ../integration-tests/
-PYTHONDONTWRITEBYTECODE=1 python3 "${DIR}"/taas.py
+PYTHONDONTWRITEBYTECODE=1 python3 "${DIR}"/taas.py "${DIR}"/enabled_tests.txt
 popd
