a0eb0255afe071cd4c293e7b8d5139bc064cea28
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-11-23 12:49:05 +0100

    Script to run job checker

---
+++ job-checker/job_checker.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -x
+
+function prepare_venv() {
+	# we want tests to run on python3.6
+	printf 'checking alias `python3.6` ... ' >&2
+	PYTHON=$(which python3.6 2> /dev/null)
+	if [ "$?" -ne "0" ]; then
+		printf "${YELLOW} NOT FOUND ${NORMAL}\n" >&2
+
+		printf 'checking alias `python3` ... ' >&2
+		PYTHON=$(which python3 2> /dev/null)
+
+		let ec=$?
+		[ "$ec" -ne "0" ] && printf "${RED} NOT FOUND ${NORMAL}\n" && return $ec
+	fi
+
+	printf "${GREEN} OK ${NORMAL}\n" >&2
+
+	${PYTHON} -m venv "venv" && source venv/bin/activate && pip install pycodestyle -r requirements.txt
+}
+
+python3 -B src/job_checker.py $@
