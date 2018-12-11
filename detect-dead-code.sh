426065780562486aed43ca98337b76a79453f6c6
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-12-11 11:00:27 +0100

    New subproject to check

--- detect-dead-code.sh
+++ detect-dead-code.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-directories="integration-tests ui-tests perf-tests dashboard reproducers load-tests db-integrity-tests taas baf"
+directories="integration-tests ui-tests perf-tests dashboard reproducers load-tests db-integrity-tests taas baf e2e_tests_bot"
 pass=0
 fail=0
 
