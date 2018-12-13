9db9885a26e1190721433261bcc7ed7fccf24402
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-12-13 09:46:01 +0100

    Two new CLI options: CSV output + dry run

--- baf/src/cliargs.py
+++ baf/src/cliargs.py
@@ -8,6 +8,14 @@ cli_parser.add_argument('-u', '--url',
                         help='specify URL to test',
                         action='store')
 
+cli_parser.add_argument('-D', '--dry',
+                        help='dry run (do not call REST API)',
+                        action='store_true')
+
 cli_parser.add_argument('-H', '--html',
                         help='generate report in HTML format',
+                        action='store_true')
+
+cli_parser.add_argument('-C', '--csv',
+                        help='generate report in CSV format',
                         action='store_true')
