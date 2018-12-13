16aaad4788afab85a4548739862b06d80ae7a445
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-12-13 12:48:34 +0100

    Add missing import

--- baf/src/fuzzer.py
+++ baf/src/fuzzer.py
@@ -42,19 +42,19 @@ def construct_url(test):
     return url
 
 
-def perform_test(url, dry_run, payload, cfg, expected_status):
+def perform_test(url, http_method, dry_run, payload, cfg, expected_status):
     if dry_run:
         log.info("(dry run)")
         pprint(payload)
     else:
-        if test["Method"] == "POST":
+        if http_method == "POST":
             log.info("POSTing data")
             response = send_payload(url, payload, cfg["access_token"])
             assert response.status_code == expected_status
 
 
-def run_tests_with_removed_items_one_iteration(url, dry_run, original_payload, cfg,
-                                               expected_status, items_count, remove_flags):
+def run_tests_with_removed_items_one_iteration(url, http_method, dry_run, original_payload,
+                                               cfg, expected_status, items_count, remove_flags):
     """One iteration for the run_tests_with_removed_items()."""
     keys = list(original_payload.keys())
     # deep copy
@@ -65,10 +65,11 @@ def run_tests_with_removed_items_one_iteration(url, dry_run, original_payload, c
             key = keys[i]
             log.info("Removing item #{n} with key '{k}' from payload".format(n=i, k=key))
             del new_payload[key]
-    perform_test(url, dry_run, new_payload, cfg, expected_status)
+    perform_test(url, http_method, dry_run, new_payload, cfg, expected_status)
 
 
-def run_tests_with_removed_items(url, dry_run, original_payload, cfg, expected_status):
+def run_tests_with_removed_items(url, http_method, dry_run, original_payload, cfg,
+                                 expected_status):
     """Run tests with items removed from the original payload."""
     iteration = 0
     with log.indent():
@@ -83,8 +84,9 @@ def run_tests_with_removed_items(url, dry_run, original_payload, cfg, expected_s
             log.info("Iteration #{n}".format(n=iteration))
             with log.indent():
                 for remove_flags in remove_flags_list:
-                    run_tests_with_removed_items_one_iteration(url, dry_run, original_payload,
-                                                               cfg, expected_status, items_count,
+                    run_tests_with_removed_items_one_iteration(url, http_method, dry_run,
+                                                               original_payload, cfg,
+                                                               expected_status, items_count,
                                                                remove_flags)
             iteration += 1
 
@@ -93,6 +95,9 @@ def run_test(cfg, test):
     """Run one selected test."""
     url = construct_url(test)
     log.info("URL to test: " + url)
+
+    http_method = test["Method"]
+    log.info("HTTP method:   " + http_method)
 
     dry_run = cfg["dry_run"]
     add_items = yes_no(test["Add items"])
@@ -109,10 +114,11 @@ def run_test(cfg, test):
     log.info("Original payload file:      " + filename)
 
     original_payload = load_json(filename)
-    perform_test(url, dry_run, original_payload, cfg, expected_status)
+    perform_test(url, http_method, dry_run, original_payload, cfg, expected_status)
 
     if remove_items:
         log.info("Run tests with items removed from original payload")
-        run_tests_with_removed_items(url, dry_run, original_payload, cfg, expected_status)
+        run_tests_with_removed_items(url, http_method, dry_run, original_payload, cfg,
+                                     expected_status)
 
     log.success("Finished")
