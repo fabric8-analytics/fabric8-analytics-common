fff3b0b86d61b003d87742ad12cccd058e4d58fb
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-01-04 09:44:24 +0100

    All output files can be specified on the CLI

--- baf/src/cliargs.py
+++ baf/src/cliargs.py
@@ -12,10 +12,22 @@ cli_parser.add_argument('-D', '--dry',
                         help='dry run (do not call REST API)',
                         action='store_true')
 
+cli_parser.add_argument('-t', '--text',
+                        help='generate report in plain text format',
+                        action='store')
+
 cli_parser.add_argument('-H', '--html',
                         help='generate report in HTML format',
-                        action='store_true')
+                        action='store')
 
 cli_parser.add_argument('-C', '--csv',
                         help='generate report in CSV format',
-                        action='store_true')
+                        action='store')
+
+cli_parser.add_argument('-T', '--tsv',
+                        help='generate report in TSV format',
+                        action='store')
+
+cli_parser.add_argument('-X', '--xml',
+                        help='generate report in XML format',
+                        action='store')
