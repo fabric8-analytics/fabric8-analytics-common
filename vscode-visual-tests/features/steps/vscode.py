f8fc4f4fd8b165bef1bf72b954b6ff128c4fb983
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-02-08 11:48:02 +0100

    Stub for module with VS Code test steps

---
+++ vscode-visual-tests/features/steps/vscode.py
@@ -0,0 +1,6 @@
+"""Visual Studio Code-related test steps."""
+
+from time import sleep
+from behave import given, then, when
+from src.gui import perform_click_on_the_region, perform_find_the_region, perform_type
+from src.gui import perform_move_mouse_cursor
