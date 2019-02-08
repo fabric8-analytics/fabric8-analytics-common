a137b19526db0f174cbb2b98793845e23cbce879
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-02-08 11:56:46 +0100

    Removed unused import

--- vscode-visual-tests/features/steps/vscode.py
+++ vscode-visual-tests/features/steps/vscode.py
@@ -1,7 +1,7 @@
 """Visual Studio Code-related test steps."""
 
 from time import sleep
-from behave import given, then, when
+from behave import then, when
 from src.gui import perform_click_on_the_region, perform_find_the_region, perform_type
 from src.gui import perform_move_mouse_cursor
 
