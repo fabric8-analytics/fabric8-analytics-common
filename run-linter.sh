e2b3c608c36b309e29675b69b3872f34452989bb
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-04-15 09:33:03 +0200

    All scripts should use Python 3.6 instead of Python 3.4

--- run-linter.sh
+++ run-linter.sh
@@ -7,12 +7,12 @@ fail=0
 function prepare_venv() {
     VIRTUALENV=$(which virtualenv)
     if [ $? -eq 1 ]; then
-        # python34 which is in CentOS does not have virtualenv binary
+        # python36 which is in CentOS does not have virtualenv binary
         VIRTUALENV=$(which virtualenv-3)
     fi
     if [ $? -eq 1 ]; then
-        # still don't have virtual environment -> use python3.4 directly
-        python3.4 -m venv venv && source venv/bin/activate && python3 "$(which pip3)" install pycodestyle
+        # still don't have virtual environment -> use python3.6 directly
+        python3.6 -m venv venv && source venv/bin/activate && python3 "$(which pip3)" install pycodestyle
     else
         ${VIRTUALENV} -p python3 venv && source venv/bin/activate && python3 "$(which pip3)" install pycodestyle
     fi
