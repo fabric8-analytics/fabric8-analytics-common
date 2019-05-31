41ebb6ec1f6296804862d99e0c0746136e4be678
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-05-31 09:05:36 +0200

    Refactoring: find location

--- vscode-visual-tests/features/src/gui.py
+++ vscode-visual-tests/features/src/gui.py
@@ -47,8 +47,15 @@ def region_filename_in_directory(directory, version, region):
     return filename
 
 
+def entry_region_check(context, region):
+    """Check if context and region are set."""
+    assert context is not None, "Context is not set (FATAL)"
+    assert region is not None, "Name of region is required parameter"
+
+
 def filename_for_region(context, region):
     """Proper filename for file containing pattern for region."""
+    assert context is not None, "Context is not set (FATAL)"
     assert region is not None, "Name of region is required parameter"
 
     version = context.vs_code_version
@@ -58,6 +65,7 @@ def filename_for_region(context, region):
 
 def save_screenshot(context, region):
     """Save screenshot with the filename the same as the region."""
+    assert context is not None, "Context is not set (FATAL)"
     assert region is not None, "Name of region is required parameter"
 
     version = context.vs_code_version
@@ -68,7 +76,7 @@ def save_screenshot(context, region):
 
 def perform_find_the_region(context, region, alternate_region=None):
     """Try to find region on screen based on specified pattern."""
-    assert region is not None, "Name of region is required parameter"
+    entry_region_check(context, region)
     context.location = None
 
     try:
@@ -82,7 +90,7 @@ def perform_find_the_region(context, region, alternate_region=None):
             try:
                 filename = filename_for_region(context, alternate_region)
                 location = context.pyautogui.locateOnScreen(filename)
-                assert location is not None
+                check_location_existence(location)
             except Exception:
                 save_screenshot(context, alternate_region)
                 raise Exception("Alternate region '{r}' can not be found on the screen".format(
