a334823a8cfcdc299d2e81f61b7b81179de7cdab
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-07-10 09:39:11 +0200

    Fix for the issue #962

--- integration-tests/features/src/stack_analysis_common.py
+++ integration-tests/features/src/stack_analysis_common.py
@@ -1,11 +1,16 @@
 """Common functions used to test stack analysis API."""
 
 
-def contains_alternate_node(json_resp):
-    """Check for the existence of alternate node in the stack analysis."""
+def get_result_from_payload(json_resp):
+    """Try to get result node from the payload."""
     assert json_resp is not None
     assert 'result' in json_resp
     # read the actual result
-    result = json_resp.get('result')
+    return json_resp.get('result')
+
+
+def contains_alternate_node(json_resp):
+    """Check for the existence of alternate node in the stack analysis."""
+    result = get_result_from_payload(json_resp)
     return bool(result) and isinstance(result, list) \
         and (result[0].get('recommendation', {}) or {}).get('alternate', None) is not None
