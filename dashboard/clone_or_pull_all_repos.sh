dbb9779f84a90d73160069d6a9bd322facfae814
Meer Sawood <msawood@redhat.com>
2020-08-19 19:35:28 +0530

    removed some more

--- dashboard/clone_or_pull_all_repos.sh
+++ dashboard/clone_or_pull_all_repos.sh
@@ -11,23 +11,18 @@ fabric8-analytics-data-model
 fabric8-analytics-server
 fabric8-analytics-jobs
 fabric8-analytics-license-analysis
-fabric8-analytics-tagger
 fabric8-analytics-worker
-fabric8-analytics-nvd-toolkit
 fabric8-analytics-auth
 fabric8-gemini-server
 fabric8-analytics-version-comparator
 fabric8-analytics-npm-insights
-fabric8-analytics-notification-scheduler
 fabric8-analytics-utils
-fabric8-analytics-release-monitor
 fabric8-analytics-github-refresh-cronjob
 f8a-server-backbone
 f8a-hpf-insights
 f8a-stacks-report
 f8a-data-ingestion-service
 f8a-emr-deployment
-cvejob
 fabric8-analytics.github.io
 fabric8-analytics-lsp-server"
 
